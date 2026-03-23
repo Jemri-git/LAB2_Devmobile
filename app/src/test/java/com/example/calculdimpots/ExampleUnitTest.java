8bebdd279f8ea7fffef5d2816622c82ac42d1602
Jemri-git <maryam.ikherazen@gmail.com>
2026-03-23 03:00:49 +0100

    5_lab2

--- app/src/test/java/com/example/calculdimpots/ExampleUnitTest.java
+++ app/src/test/java/com/example/calculdimpots/ExampleUnitTest.java
@@ -11,7 +11,37 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testImpotBase() {
+        // Context of the app under test.
+        double surface = 120;
+        double impotBase = surface * 2;
+        assertEquals(240.0, impotBase, 0.0);
+    }
+
+    @Test
+    public void testImpotAvecPiscine() {
+        int pieces = 4;
+        boolean pool = true;
+        int supplement = pieces * 50 + (pool ? 100 : 0);
+        assertEquals(300, supplement);
+    }
+    @Test
+    public void testImpotSansPiscine() {
+        int pieces = 4;
+        boolean pool = false;
+        int supplement = pieces * 50 + (pool ? 100 : 0);
+        assertEquals(200, supplement);
+    }
+    @Test
+    public void testImpotTotal() {
+        double surface = 120;
+        int pieces = 4;
+        boolean pool = true;
+
+        double base       = surface * 2;        // 240
+        int supplement    = pieces * 50 + 100;  // 300
+        double total      = base + supplement;  // 540
+
+        assertEquals(540.0, total, 0.0);
     }
 }
